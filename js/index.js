d9eaaf3e61d41025bbde226cd4f926810d7179bf
joeld-c <36539467+joeld-c@users.noreply.github.com>
2018-05-10 12:06:41 +0100

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -22,20 +22,3 @@ $('#main-slider').slick({
     $('#main-slider .slick-slide').find('video').get(0).pause();
     var video = $('#main-slider .slick-active').find('video').get(0).play();
 });
-
-var path = document.querySelector('#star-path');
-var length = path.getTotalLength();
-// Clear any previous transition
-path.style.transition = path.style.WebkitTransition =
-  'none';
-// Set up the starting positions
-path.style.strokeDasharray = length + ' ' + length;
-path.style.strokeDashoffset = length;
-// Trigger a layout so styles are calculated & the browser
-// picks up the starting position before animating
-path.getBoundingClientRect();
-// Define our transition
-path.style.transition = path.style.WebkitTransition =
-  'stroke-dashoffset 2s ease-in-out';
-// Go!
-path.style.strokeDashoffset = '0';
